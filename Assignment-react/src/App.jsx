f50dcaf1ab6b480780b27fa9b58322eb61d64f8f
Deovrat Tiwari <tdeovrat@gmail.com>
2024-01-18 03:16:14 +0530

    mobile added to dowloadApp component

--- Assignment-react/src/App.jsx
+++ Assignment-react/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
   <>
     <FoodMenu/>
+    <br></br>
     <DownloadApp/>
   </>
   )
--- Assignment-react/src/pages/DownloadApp.jsx
+++ Assignment-react/src/pages/DownloadApp.jsx
@@ -5,6 +5,10 @@ import { Typography } from '@mui/material';
 // import playstore from '../assets/'
 import playstore from '../assets/google-play-badge.png'
 import appstore from '../assets/App_Store_Badge.png'
+import appScreen from '../assets/app-screen.png'
+import cauliflower from '../assets/1cauliflower.png'
+import dotsBg from '../assets/dots-bg1.png'
+
 
 const DownloadApp = () => {
   return (
@@ -26,12 +30,26 @@ const DownloadApp = () => {
         </Typography>
         </Grid>
         <Box sx={{ width: '30px',display: 'flex', flexDirection: 'row', ml: 3, mt: 2 }}>
-            <img src= {playstore} />
-            <img src= {appstore} />
+            <img src= {playstore} alt='app-download-android' />
+            <img src= {appstore}  alt='app-download-apple'/>
         </Box>
       </Grid>
-      <Grid item xs={5}>
-        <Typography>hi</Typography>
+      <Grid item xs={6} style={{ position: 'relative' }}>
+        <img
+          style={{ position: 'absolute', top: 0, left: 140, zIndex: 2, height: '700px', width: '400px' }}
+          src={appScreen}
+          alt='app-screen'
+        />
+        <img
+          style={{ position: 'absolute', top: 230, left: 20, zIndex: 1, height: '400px' }}
+          src={cauliflower}
+          alt='cauliflower'
+        />
+         <img
+          style={{ position: 'absolute', top: 115, left: 110, zIndex: 1, height: '300px' }}
+          src={dotsBg}
+          alt='cauliflower'
+        />
       </Grid>
     <Grid item xs={1}></Grid>
 
