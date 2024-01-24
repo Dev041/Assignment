4557d61865847e66a35aea4b2e1bdfaeee8f14dd
Deovrat Tiwari <tdeovrat@gmail.com>
2024-01-24 19:20:50 +0530

    ourServices changes

--- Assignment-react/src/pages/HeroComponent.jsx
+++ Assignment-react/src/pages/HeroComponent.jsx
@@ -1,40 +1,55 @@
 import React from 'react';
-import { Avatar, Box, Grid, Typography, InputBase, Button } from '@mui/material';
+import { Avatar, Box, Grid, Typography, InputBase, Button, Stack } from '@mui/material';
 import userpic from '../assets/userpic.png';
 import dotsbg from '../assets/dots-bg.svg';
 import lettuce from '../assets/pngtree—lettuce.png';
 import salad from '../assets/Pngtree-delicious-food.png';
 import food from '../assets/food.svg';
 import location from '../assets/place-holder.svg';
+import logo from '../assets/logo.svg';
+import arrow1 from '../assets/left-arrow.svg';
+import arrow2 from '../assets/right-arrow.svg';
+
 
 const HeroComponent = () => {
   return (
     <Box sx={{ height: '100vh' }}>
       <Grid container height="100%" justifyContent="center" alignItems="center" >
+        <Grid item xs={6} sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'left', position: 'absolute', top: 0, left: 150, width: '100%', zIndex: 4, pt: 2, pb: 2 }}>
+          <Box>
+            <img src={logo} alt="Logo" height="60px" />
+          </Box>
+          <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+            <Typography variant="subtitle1" fontWeight={700} color="#2E266F" sx={{ mr: 4 }}>Menu One</Typography>
+            <Typography variant="subtitle1" fontWeight={700} color="#2E266F" sx={{ mr: 4 }}>Menu Two</Typography>
+            <Typography variant="subtitle1" fontWeight={700} color="#2E266F" sx={{ mr: 4 }}>Menu Three</Typography>
+            <Typography variant="subtitle1" fontWeight={700} color="#2E266F">Menu Four</Typography>
+          </Box>
+        </Grid>
         {/* Grid 1 (Size: 1) */}
         <Grid item xs={12} sm={1.5}>
-        <Box 
-          sx={{
-            width: '200px',
-            height: '50px',
-            ml: 5,
-            mt: '80vh',
-            bgcolor: '#3D7475',
-            borderRadius: '0 25px 25px 0',
-          }}
-        >
-          <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'right'  }}>
-            <img alt="location image" src={location} style={{height: '30px', marginTop: '10px'}} />
-            <Typography sx={{ color: "white", ml: 1.5, fontSize: '1.5rem', mr: 2, mt: '10px' }}>
-              Indore
-            </Typography>
+          <Box
+            sx={{
+              width: '200px',
+              height: '50px',
+              ml: 5,
+              mt: '80vh',
+              bgcolor: '#3D7475',
+              borderRadius: '0 25px 25px 0',
+            }}
+          >
+            <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'right' }}>
+              <img alt="location image" src={location} style={{ height: '30px', marginTop: '10px' }} />
+              <Typography sx={{ color: "white", ml: 1.5, fontSize: '1.5rem', mr: 2, mt: '10px' }}>
+                Indore
+              </Typography>
+            </Box>
           </Box>
-        </Box>
         </Grid>
 
         {/* Grid 2 (Size: 5) */}
         <Grid item xs={12} sm={5}>
-          <Box sx={{ height: '100%', mt:0 , textAlign: 'left', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start' }}>
+          <Box sx={{ height: '100%', mt: 0, textAlign: 'left', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start' }}>
             <img src={food} alt='food-img' width="60%" height="35%" />
             <Typography variant='h3' color="#2E266F" fontWeight={700}>
               Discover Restaurant & Delicious Food
@@ -61,11 +76,11 @@ const HeroComponent = () => {
               />
               <Button
                 variant="contained"
-                sx={{ 
+                sx={{
 
                   backgroundColor: "#3D7475",
                   color: "white",
-                  mr: 1, 
+                  mr: 1,
                   width: '130px',
                   height: '3.2rem',
                   borderRadius: '32px 0 0 0',
@@ -73,7 +88,7 @@ const HeroComponent = () => {
                   fontSize: '25px',
                   '&:hover': {
                     backgroundColor: "#3D7475"
-                  } 
+                  }
                 }}
               >
                 Go
@@ -84,36 +99,48 @@ const HeroComponent = () => {
 
         {/* Grid 3 */}
         <Grid item xs={12} sm={2.5} sx={{ backgroundColor: '#fff', height: '100%' }}>
-        <img
+          <img
             src={dotsbg}
             alt="Image 1"
             style={{ position: 'absolute', top: 90, height: '80%', right: 35, width: '65%', zIndex: 1 }}
           />
-          
+
           {/* Image 2 (On top of Image 1) */}
           <img
             src={lettuce}
             alt="Image 2"
-            style={{ position: 'absolute', height: '90%', width: '30%', right: 140 , top: -55 , zIndex: 2 }}
+            style={{ position: 'absolute', height: '90%', width: '30%', right: 140, top: -55, zIndex: 2 }}
           />
-          
+
           {/* Image 3 (On top of Image 2) */}
           <img
             src={salad}
             alt="Image 3"
-            style={{ position: 'absolute', bottom: 0, right: 80 ,width: '40%', top: 90, height: '85%', zIndex: 3 }}
+            style={{ position: 'absolute', bottom: 0, right: 80, width: '40%', top: 90, height: '85%', zIndex: 3 }}
           />
         </Grid>
 
         {/* Grid 4 */}
-        <Grid item xs={12} sm={3} sx={{ backgroundColor: '#3d7475', height: '100%'}}>
-          <Box sx={{ padding: 2, display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center',  }}>
+        <Grid item xs={12} sm={3} sx={{ backgroundColor: '#3d7475', height: '100%' }}>
+          <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', position: 'absolute', top: 10, ml: 0, width: '330px', zIndex: 4, }}>
             <Avatar alt="user pic" src={userpic} />
             <Typography sx={{ color: "white", ml: 2 }}>
-              User 
+              User
               <span style={{ color: 'white', fontSize: '0.6rem', verticalAlign: 'middle', marginLeft: 2 }}> ▼</span>
             </Typography>
-          </Box>
+            </Box>
+            <Box
+              sx={{
+                // mt: '80vh',
+                mt: '90vh',
+                ml: 15,
+                display: 'flex',
+                flexDirection: 'row'
+              }}
+            >
+                <img src={arrow1} height="40px" style={{marginRight: 20}} />
+                <img src={arrow2} height="40px" />
+            </Box>
         </Grid>
       </Grid>
     </Box>
--- Assignment-react/src/pages/OurServices.jsx
+++ Assignment-react/src/pages/OurServices.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Box, Button, Grid, Stack, Typography } from '@mui/material';
-import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 import delivery from '../assets/free-home-delivery-at-your-door-steps.svg';
 import dotsbg from '../assets/dots-bg2.svg';
-import arrow from '../assets/arrow2.svg';
+import advancebooking from '../assets/advanced-table-booking.svg';
+import foodfree from '../assets/free-home-delivery-at-your-door-steps.svg';
+import arrow from '../assets/big-left-arrow.svg';
 
 const DineIn = () => {
     return (
@@ -38,11 +39,44 @@ const DineIn = () => {
                 {/* Grid 4 (Size: 1) */}
                 <Grid item xs={12} sm={2}></Grid>
             </Grid>
-            <Grid container sx={{ height: '70%', pl: 4, backgroundImage: `url(${dotsbg})`, backgroundSize: 'cover' }}>
+            <Grid container sx={{ height: '70%', backgroundImage: `url(${dotsbg})`, backgroundSize: 'cover' }}>
                 <Grid item xs={12} sm={2}></Grid>
                 <Grid container xs={12} sm={9}>
                     <Grid item xs={7}>
-                        <Typography>Hi</Typography>
+                        <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                            <Box sx={{ background: '#FAFAFA', width: '200px', height: '200px', mt: 2.5 }}>
+                                <img src={advancebooking} style={{ position: 'absolute', width: '150px', height: '150px', marginLeft: '30px', marginTop: '20px' }} />
+                                <img src={arrow} style={{ position: 'relative', zIndex: 2, top: '40%', left: '180px', width: '5rem' }} />
+                            </Box>
+                            <Box sx={{ width: '250px', ml: 10, mt: 3,  display: 'flex', flexDirection: 'column'}}>
+                                <Typography variant='h4' fontWeight={500}>
+                                    advance table booking
+                                </Typography>
+                                <Typography sx={{ width: '170px', mb: 2, color: '#707070', fontWeight: '600' }}> Lorem ipsum dolor sit amet consectetur elit sit amet</Typography>
+
+                                <Box sx={{ display: 'flex', flexDirection: 'row', mt: 0.5 }}>
+                                    <Box sx={{ width: '180px', height: '3px', background: '#3D7475', borderRadius: 4, mr: 0.5 }}></Box>
+                                    <Box sx={{ width: '30px', height: '3px', background: '#3D7475', borderRadius: 4 }}></Box>
+                                </Box>
+                            </Box>
+                        </Box>
+                        <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                            <Box sx={{ width: '250px', height: '200px', mt: 3, display: 'flex', flexDirection: 'column'}}>
+                                <Typography variant='h4' fontWeight={500}>
+                                    food for free
+                                </Typography>
+                                <Typography sx={{ width: '170px', mb: 2, color: '#707070', fontWeight: '600' }}> Lorem ipsum dolor sit amet consectetur elit sit amet</Typography>
+
+                                <Box sx={{ display: 'flex', flexDirection: 'row', mt: 0.5 }}>
+                                    <Box sx={{ width: '140px', height: '3px', background: '#3D7475', borderRadius: 4, mr: 0.5 }}></Box>
+                                    <Box sx={{ width: '30px', height: '3px', background: '#3D7475', borderRadius: 4 }}></Box>
+                                </Box>
+                            </Box>
+                            <Box sx={{ background: '#FAFAFA', width: '230px', height: '200px', mt: 2.5, ml:3 }}>
+                                <img src={foodfree} style={{ position: 'absolute', width: '150px', height: '150px', marginLeft: '40px' }} />
+                                <img src={arrow} style={{ position: 'relative', zIndex: 2, top: '35%', left: '-80px', width: '5rem',  transform: 'rotate(180deg)' }} />
+                            </Box>
+                        </Box>
                     </Grid>
                     <Grid item xs={4}>
                         <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', bgcolor: '#FAFAFA', width: '100%', height: '100%', mt: 2.5 }}>
